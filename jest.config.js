d47da1343e39bb48e94a93d8e2137e66f67a6fc9
luisherreralg <lherreralg@outlook.es>
2022-10-20 17:22:53 +0200

    Initial commit

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+const preset = 'ts-jest';
+const testEnvironment = 'node';
+const testPathIgnorePatterns = ['dist'];
+
+export default { preset, testEnvironment, testPathIgnorePatterns };
